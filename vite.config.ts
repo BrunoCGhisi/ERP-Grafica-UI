3a4527d2e18dd926b218695efb4af1b3574f95bf
MaJoApol <mariajoanaceron@gmail.com>
2024-11-18 11:28:46 -0300

    arrunei desgraça

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), basicSsl()],
+  server: {
+    https: false, // Desativa o SSL
+  },
+  optimizeDeps: {
+    include: ['@mui/material/Tooltip', '@emotion/styled'],
+  },
+});
