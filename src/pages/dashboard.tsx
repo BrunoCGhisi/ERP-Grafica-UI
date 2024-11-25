2665561e162e51f4a45a24232edce384f12d7902
Bruno Costa Ghisi <brunocostaghisi@gmail.com>
2024-11-25 01:08:02 -0300

    dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,9 @@ import { MiniDrawer } from "../shared/components";
 import { SpaceStyle } from "../shared/styles";
 import { getToken } from "../shared/services/payload";
 import PieMostProduct from "./pieTeste";
-
+import { flexbox } from "@mui/system";
+import UnarchiveIcon from "@mui/icons-material/Unarchive";
+import ArchiveIcon from "@mui/icons-material/Archive";
 const Dashboard = () => {
   const navigate = useNavigate();
   const [userId, setUserId] = useState<number | null>(null);
@@ -37,7 +39,9 @@ const Dashboard = () => {
 
     const fetchResumo = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/financeiro/resumo");
+        const response = await axios.get(
+          "http://localhost:3000/financeiro/resumo"
+        );
         setResumo(response.data);
       } catch (error) {
         console.error("Erro ao buscar resumo financeiro:", error);
@@ -59,23 +63,56 @@ const Dashboard = () => {
 
           {/* Valores A pagar e A receber */}
           <Grid container item xs={12} spacing={2}>
-            <Grid item xs={6}>
-              <Typography variant="h6">📤 A Pagar</Typography>
-              <Typography variant="body1" color="error">
-                R$ {resumo.totalPagar.toFixed(2)}
-              </Typography>
-              <Typography variant="body2">
-                Contas: {resumo.qtdContasPagar}
-              </Typography>
+            <Grid item xs={3}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  padding: 2,
+                  borderRadius: 2,
+                  boxShadow: 2,
+                }}
+              >
+                <Typography variant="h4" color="error" sx={{ ml: 2 }}>
+                  {resumo.qtdContasPagar}
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "column", ml: 2 }}>
+                  <Typography variant="h6">Contas a Pagar</Typography>
+                  <Typography variant="body1" color="error">
+                    Total R$ {resumo.totalPagar.toFixed(2)}
+                  </Typography>
+                </Box>
+                <Box sx={{ color: "#f00" }}>
+                  <UnarchiveIcon sx={{ ml: 4, fontSize: 40 }} />
+                </Box>
+              </Box>
             </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6">📥 A Receber</Typography>
-              <Typography variant="body1" color="primary">
-                R$ {resumo.totalReceber.toFixed(2)}
-              </Typography>
-              <Typography variant="body2">
-                Contas: {resumo.qtdContasReceber}
-              </Typography>
+
+            <Grid item xs={3}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  padding: 2,
+                  borderRadius: 2,
+                  boxShadow: 2,
+                }}
+              >
+                <Typography variant="h4" color="error" sx={{ ml: 2 }}>
+                  {resumo.qtdContasReceber}
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "column", ml: 2 }}>
+                  <Typography variant="h6">Contas a Receber</Typography>
+                  <Typography variant="body1" color="error">
+                    Total R$ {resumo.totalReceber.toFixed(2)}
+                  </Typography>
+                </Box>
+                <Box sx={{ color: "#0f0" }}>
+                  <UnarchiveIcon sx={{ ml: 4, fontSize: 40 }} />
+                </Box>
+              </Box>
             </Grid>
           </Grid>
 
